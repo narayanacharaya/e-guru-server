1abf6bcfdfee18c2657ee57d454dcec7b2a1283d
narayanacharaya <acharayanarayan01@gmail.com>
2023-04-30 20:18:27 +0545

    validting category

--- middlewares/signupvalidation.js
+++ middlewares/signupvalidation.js
@@ -6,7 +6,8 @@ const validtor =Joi.object({
   password: Joi.string().min(5).max(10).pattern(/^(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])(?=.*\d)[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{5,10}$/).required().messages({
     'string.pattern.base': 'Password must be 5-10 characters long and contain at least one special character and one number',
   }),
-  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({ 'any.only': 'Passwords must match' })
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({ 'any.only': 'Passwords must match' }),
+  category: Joi.string().valid('user', 'publisher').required(),
 });
 const signupvalidtormiddleware=(req,res,next)=>{
     console.log("im called");
@@ -19,7 +20,7 @@ const signupvalidtormiddleware=(req,res,next)=>{
           });
     }
     else{
-        res.send("everyting is all right ")
+        next();
     }
 }
 module.exports = signupvalidtormiddleware;
